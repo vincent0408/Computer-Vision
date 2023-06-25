89c22aac387543f69863cb597253dbdba211994c
vincent0408 <tnecniv43208@gmail.com>
2023-06-25 11:54:58 +0800

    cleanup

--- Final/visual.py
+++ Final/visual.py
@@ -5,10 +5,10 @@ import csv
 import matplotlib.pyplot as plt
 
 
-path = './seq1/dataset/'
+path = './ITRI_dataset/seq1/dataset/'
 folders = sorted(glob(path + '*'))
 
-first_folder = folders[1]
+first_folder = folders[3]
 
 image = cv2.imread(first_folder + '/raw_image.jpg').astype(np.uint8)
 
@@ -22,6 +22,6 @@ for x1, y1, x2, y2, class_id, probability in coord:
     image = cv2.circle(image, (int(float(x1)), int(float(y1))), radius=10, color=(0, 255, 0), thickness=-1)
     image = cv2.circle(image, (int(float(x2)), int(float(y2))), radius=10, color=(0, 255, 0), thickness=-1)
 
-plt.imshow(image)
+plt.imshow(cv2.cvtColor(image, cv2.COLOR_BGR2RGB))
 plt.show()
 pass
